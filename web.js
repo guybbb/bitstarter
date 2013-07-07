5dbc4147444c91c069790f7da485fbf1ec9bbab1
Guy <guybal@hotmail.com>
2013-07-07 16:57:05 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require("fs");
+    content = fs.readFileSync('index.html');
+    response.send(content.toString('utf-8'));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
